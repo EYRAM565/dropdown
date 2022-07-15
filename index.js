b31c459d9681ca21dccf4fd0f207697ef7702671
EYRAM565 <raymond.tublu@yahoo.com>
2022-07-15 19:44:21 +0100

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const nav = document.querySelector('.primary-navigation');
-const tog = document.querySelector('.toggle')
+let nav = document.querySelector('.primary-navigation');
+let tog = document.querySelector('.toggle')
+let bgwrapper = document.querySelector('.wrapper');
+let arrowUpdown = document.querySelectorAll('.arrow')
+
+for(let i = 0; i < arrowUpdown.length; i++){
+  arrowUpdown[i].addEventListener('click', ()=>{
+    arrowUpdown[i].classList.toggle('arrow-up')
+  })
+   
+}
 
 tog.addEventListener('click', ()=>{
     console.log('testing!!!');
     const visibility = nav.getAttribute('data-visible')
     
     if(visibility === 'false'){
+        bgwrapper.classList.add('wrapper-bgColor')
         nav.setAttribute('data-visible', true)
         tog.setAttribute('aria-expanded', true)
        
     }else {
+        bgwrapper.classList.remove('wrapper-bgColor')
         nav.setAttribute('data-visible', false)
         tog.setAttribute('aria-expanded', false)
     }
