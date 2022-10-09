8264be688e35f86edad646dafa012ae556c45c01
EYRAM565 <raymond.tublu@yahoo.com>
2022-10-09 06:38:30 +0100

    changes

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-let nav = document.querySelector('.primary-navigation');
-let tog = document.querySelector('.toggle')
-let bgwrapper = document.querySelector('.wrapper');
-let arrowUpdown = document.querySelectorAll('.arrow')
+// let nav = document.querySelector('.primary-navigation');
+// let tog = document.querySelector('.toggle')
+// let bgwrapper = document.querySelector('.wrapper');
+// let arrowUpdown = document.querySelectorAll('.arrow')
 
-for(let i = 0; i < arrowUpdown.length; i++){
-  arrowUpdown[i].addEventListener('click', ()=>{
-    arrowUpdown[i].classList.toggle('arrow-up')
-  })
+// for(let i = 0; i < arrowUpdown.length; i++){
+//   arrowUpdown[i].addEventListener('click', ()=>{
+//     arrowUpdown[i].classList.toggle('arrow-up')
+//   })
    
-}
+// }
 
-tog.addEventListener('click', ()=>{
-    console.log('testing!!!');
-    const visibility = nav.getAttribute('data-visible')
+// tog.addEventListener('click', ()=>{
+//     console.log('testing!!!');
+//     const visibility = nav.getAttribute('data-visible')
     
-    if(visibility === 'false'){
-        bgwrapper.classList.add('wrapper-bgColor')
-        nav.setAttribute('data-visible', true)
-        tog.setAttribute('aria-expanded', true)
+//     if(visibility === 'false'){
+//         bgwrapper.classList.add('wrapper-bgColor')
+//         nav.setAttribute('data-visible', true)
+//         tog.setAttribute('aria-expanded', true)
        
+//     }else {
+//         bgwrapper.classList.remove('wrapper-bgColor')
+//         nav.setAttribute('data-visible', false)
+//         tog.setAttribute('aria-expanded', false)
+//     }
+// })
+
+
+const toggle = document.querySelector('.toggle');
+const navigation = document.querySelector('nav');
+let bgwrapper = document.querySelector('header');
+
+toggle.addEventListener('click', ()=>{
+    console.log('testing.... testing!!!!');
+    const visibility = navigation.getAttribute('data-visible')
+    if(visibility === 'false'){
+      bgwrapper.classList.add('wrapper-bgColor')
+        navigation.setAttribute('data-visible', true)
+        toggle.setAttribute('aria-expanded', true)
     }else {
-        bgwrapper.classList.remove('wrapper-bgColor')
-        nav.setAttribute('data-visible', false)
-        tog.setAttribute('aria-expanded', false)
+      bgwrapper.classList.remove('wrapper-bgColor')
+        navigation.setAttribute('data-visible', false)
+        toggle.setAttribute('aria-expanded', false)
     }
 })
